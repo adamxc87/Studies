b9994a1e9ad44fa4aa7e053d39ded2cea9b1647f
adam <bubaladam@gmail.com>
2025-05-28 21:54:50 +0200

    task4
    4. Speciefied operation results

--- Informatyka_Zajecia_IV/Program.cs
+++ Informatyka_Zajecia_IV/Program.cs
@@ -23,6 +23,14 @@ namespace Informatyka_Zajecia_IV
             int result = 12 / 2;
             Console.WriteLine($"SUM: {result}");
             Console.WriteLine();
+
+            // 4. Divide Two Numbers
+            Console.WriteLine("ZADANIE 4.");
+            Console.WriteLine(-1 + 4 * 6);
+            Console.WriteLine((35 + 5) % 7);
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+            Console.WriteLine();
         }
     }
 }
